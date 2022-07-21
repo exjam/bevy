0e877a9a69623a1bd7c6fc79c88a145e29c20aa3
James Benton <james.benton2@gmail.com>
2022-07-21 15:27:25 +0100

    Enable frustum culling far plane.

--- crates/bevy_render/src/view/visibility/mod.rs
+++ crates/bevy_render/src/view/visibility/mod.rs
@@ -222,11 +222,11 @@ pub fn check_visibility(
                         radius: (Vec3A::from(transform.scale) * model_aabb.half_extents).length(),
                     };
                     // Do quick sphere-based frustum culling
-                    if !frustum.intersects_sphere(&model_sphere, false) {
+                    if !frustum.intersects_sphere(&model_sphere, true) {
                         return;
                     }
                     // If we have an aabb, do aabb-based frustum culling
-                    if !frustum.intersects_obb(model_aabb, &model, false) {
+                    if !frustum.intersects_obb(model_aabb, &model, true) {
                         return;
                     }
                 }
